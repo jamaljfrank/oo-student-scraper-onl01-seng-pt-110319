863c0d8f85ceae4e793d8f9d6be149aa6d9e0a81
Jamal Frank <jamaljfrank@gmail.com>
2019-12-04 02:18:39 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,13 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    locations_array = []
+    url_array = []
+    names_array = []
+    final_array = []
+    html = open("#{index_url}")
+    doc = Nokogiri::HTML(html)
+
   end
 
   def self.scrape_profile_page(profile_url)
