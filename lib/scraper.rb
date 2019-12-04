4c5fdfb1a3e3a646a38c441fa1b52cdb4743b6dc
Jamal Frank <jamaljfrank@gmail.com>
2019-12-04 02:54:06 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,20 +20,22 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
-    scraped_attr = []
-    index_page = Nokogiri::HTML(open(profile_url))
-    index_page.css("div.roster-cards-container").each do |profile|
-      profile.css(".student-card a").each do |student|
-        link = student.css(".student-name").text
-        student_link = "#{student.attr('href')}"
-        student_location = student.css(".student-location").text
-
-    
-    scraped_attr << {name: student_name, location: student_location, profile_url: student_link }
+    student_profile = {}
+    profile_page = Nokogiri::HTML(open(profile_url))
+    social_media_links = profile_page.css(".social-icon-container").children.css("a").map { |sml| sml.attribute("href").value }
+    social_media_links.each do |link|
+      if link.include?("linkedin")
+        student_profile[:linkedin] = link
+      elsif link.include?("twitter")
+        student_profile[:twitter] = link
+      elsif link.include?("github")
+        student_profile[:github] = link
+      else student_profile[:blog] = link
       end
     end
-    scraped_attr
+      student_profile[:profile_quote] = profile_page.css(".profile-quote").text if profile_page.css(".profile-quote")
+      student_profile[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text if profile_page.css("div.bio-content.content-holder div.description-holder p")
+
+    student_profile
   end
-
 end
-
